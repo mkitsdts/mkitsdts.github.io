5361f1eb80df8de524665e6bd634f4fc735a79a6
mkitsdts <mkitsdts@outlook.com>
2024-10-30 17:42:30 +0800

    Site updated: 2024-10-30 17:42:30

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730210841339";
+const VERSION = "1730281349883";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
