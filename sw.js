dc15aa4e198c71c0b270aa9a934238f2ce2a1417
mkitsdts <mkitsdts@outlook.com>
2024-11-06 07:39:09 +0800

    Site updated: 2024-11-06 07:39:09

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730732475196";
+const VERSION = "1730849948966";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
