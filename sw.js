bead30b07e27bbd3314540aec1027b89aa5083d4
mkitsdts <mkitsdts@outlook.com>
2024-12-14 22:55:20 +0800

    Site updated: 2024-12-14 22:55:20

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733908285574";
+const VERSION = "1734188119492";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
